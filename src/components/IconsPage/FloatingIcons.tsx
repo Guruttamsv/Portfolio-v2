9bc070b38693d79297d9d98a403ea29dc0057d26
Guruttamsv <Guruttamsv511@gmail.com>
2025-02-13 01:59:01 +0000

    commit 11

--- src/components/IconsPage/FloatingIcons.tsx
+++ src/components/IconsPage/FloatingIcons.tsx
@@ -107,16 +107,16 @@ const FloatingIcon: React.FC<{
 // Main component to display all floating icons
 const FloatingIcons: React.FC<{ mobile?: boolean }> = ({ mobile = false }) => {
   const iconPaths = [
-    "/assets/GLTFS/AirPods/AirPods.gltf",
-    "/assets/GLTFS/Cactus/Cactus.gltf",
-    "/assets/GLTFS/Calculator/Calculator.gltf",
-    "/assets/GLTFS/Flashdrive/Flashdrive.gltf",
-    "/assets/GLTFS/Joystick/Joystick.gltf",
-    "/assets/GLTFS/Lifebuoy/Lifebuoy.gltf",
-    "/assets/GLTFS/Mouse/Mouse.gltf",
-    "/assets/GLTFS/Router/Router.gltf",
-    "/assets/GLTFS/Rubiks Cube/Rubiks Cube.gltf",
-    "/assets/GLTFS/Suitcase/Suitcase.gltf",
+    "./assets/GLTFS/AirPods/AirPods.gltf",
+    "./assets/GLTFS/Cactus/Cactus.gltf",
+    "./assets/GLTFS/Calculator/Calculator.gltf",
+    "./assets/GLTFS/Flashdrive/Flashdrive.gltf",
+    "./assets/GLTFS/Joystick/Joystick.gltf",
+    "./assets/GLTFS/Lifebuoy/Lifebuoy.gltf",
+    "./assets/GLTFS/Mouse/Mouse.gltf",
+    "./assets/GLTFS/Router/Router.gltf",
+    "./assets/GLTFS/Rubiks Cube/Rubiks Cube.gltf",
+    "./assets/GLTFS/Suitcase/Suitcase.gltf",
   ];
 
   const initialPositions: [number, number, number][] = [
